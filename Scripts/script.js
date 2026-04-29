b71167590439e8adc4f9b4b26c3591373ec292bd
João Mateus <Joao.Mateus.T0133534@edu.atec.pt>
2026-04-29 19:04:23 +0100

    Adicionada funcionalidade de login

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,20 @@
 function mostrarMensagem() {
     alert("Botão clicado!");
 }
+
+function fazerLogin() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const mensagem = document.getElementById("mensagem");
+
+    if (user === "admin" && pass === "1234") {
+        mensagem.style.color = "green";
+        mensagem.textContent = "Login com sucesso!";
+
+        document.querySelector(".login-container").style.display = "none";
+        document.getElementById("conteudo").style.display = "block";
+    } else {
+        mensagem.style.color = "red";
+        mensagem.textContent = "Credenciais erradas.";
+    }
+}
